ea647bfc0199afa9ca14db382d17d69970ef2630
JulioGroaz <julio.groaz2020@gmail.com>
2024-06-12 15:29:46 +0200

    Creazione della struttura

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/mixins' as *;
+@use './styles/partials/variables' as *;
+@use '../node_modules/bootstrap/scss/bootstrap.scss' as *;
+
+
+</style>
